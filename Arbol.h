1315ca177bc631da24747fc12512a2877a69e160
Juan Diego Rojas Valdes <judirova04@javerianacali.edu.co>
2024-08-09 15:49:30 -0500

    Agregar archivos de proyecto.

---
+++ Arbol.h
@@ -0,0 +1,14 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "glApplication.h"
+#include "glutWindow.h"
+#include "glsl.h"
+#include <time.h>
+
+class Arbol {
+
+public:
+	Arbol() {};
+
+	void dibujarArbol(float x, float y, float z);
+};
